1534966da3c40c3513af5cd431d676f3e0579c91
Daniel Lu <dandydanny@gmail.com>
2014-03-06 11:15:52 -0600

    Add event ajax start

--- public/js/application.js
+++ public/js/application.js
@@ -3,7 +3,29 @@ $(document).ready(function() {
   // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
+  $("#create-form").submit(function(event){
+  event.preventDefault();
+  console.log("create clicked");
 
 
+  var eventName = $("name").val();
+  var eventLocation = $("location").val();
+  var eventStart = $("start_time").val();
+  var eventEnd = $("end_time").val();
+
+  console.log(eventName);
+  console.log(eventLocation);
+  console.log(eventStart);
+  console.log(eventEnd);
+
+  //   $.ajax({
+  //     type: "POST",
+  //     url: "/add",
+  //     dataType: 'json',
+  //     data: {name: teaName, location: teaOpinion},
+  //   }).done(function(msg) {
+
+
+  });
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 });
